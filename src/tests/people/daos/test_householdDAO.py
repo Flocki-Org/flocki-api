84144bb6c4b07dbae3c21803793159011f21b6c7
Rowan Pillay <rowan.pillay@takealot.com>
2022-10-07 06:27:24 +0200

    Add more household_DAO tests

--- src/tests/people/daos/test_householdDAO.py
+++ src/tests/people/daos/test_householdDAO.py
@@ -10,6 +10,7 @@ from src.app.media.models.database import models as media_models
 from src.app.people.models.database import models
 # test_database.py
 from src.app.people.daos.householdDAO import HouseholdDAO
+from src.app.people.models.database.models import HouseholdImage
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
 engine = create_engine(
@@ -131,4 +132,101 @@ def test_add_household_image(test_db):
     assert len(household_queried.household_images) == 1
     assert household_queried.household_images[0].image.address == "test"
     assert household_queried.household_images[0].image.description == "test"
+
+
+def test_add_person_to_household(test_db):
+    household = models.Household(
+        leader_id=1,
+        address_id=1,
+    )
+    household = householdDAO.add_household(household)
+
+    person = models.Person(
+        first_name="test",
+        last_name="test")
+    db.add(person)
+    db.commit()
+    db.refresh(person)
+    householdDAO.add_person_to_household(household, person)
+
+    household_queried = db.query(models.Household).filter(models.Household.id == household.id).first()
+
+    assert household_queried.people is not None
+    assert len(household_queried.people) == 1
+    assert household_queried.people[0].first_name == "test"
+    assert household_queried.people[0].last_name == "test"
+
+
+def test_remove_person_from_household(test_db):
+    household = models.Household(
+        leader_id=1,
+        address_id=1,
+    )
+    db.add(household)
+    db.commit()
+    db.refresh(household)
+
+    person = models.Person(
+        first_name="test",
+        last_name="test")
+    db.add(person)
+    db.commit()
+    db.refresh(person)
+    household.people.append(person)
+
+    householdDAO.remove_person_from_household(household, person)
+
+    household_queried = db.query(models.Household).filter(models.Household.id == household.id).first()
+
+    assert household_queried.people is not None
+    assert len(household_queried.people) == 0
+
+
+def test_update_household(test_db):
+    household = models.Household(
+        leader_id=1,
+        address_id=1,
+    )
+    db.add(household)
+    db.commit()
+    db.refresh(household)
+
+    image = media_models.Image(
+        address="test",
+        description="test",
+        store="local",
+        created=datetime.now())
+
+    db.add(image)
+    db.commit()
+    db.refresh(image)
+
+    household_image = HouseholdImage(
+        household=household,
+        image=image,
+        created=datetime.now(),
+    )
+    db.add(household_image)
+    db.commit()
+
+    new_image = media_models.Image(
+        address="new_test",
+        description="new_test",
+        store="local",
+        created=datetime.now())
+
+    db.add(new_image)
+    db.commit()
+    db.refresh(new_image)
+    householdDAO.update_household(household, 2, 2, new_image)
+
+    household_queried = db.query(models.Household).filter(models.Household.id == household.id).first()
+
+    assert household_queried is not None
+    assert household_queried.leader_id == 2
+    assert household_queried.address_id == 2
+    assert household_queried.household_images is not None
+    assert len(household_queried.household_images) == 2
+    assert household_queried.household_images[1].image.address == "new_test"
+
 
